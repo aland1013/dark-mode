676f266c8cf51b7204e1f033571d737ce2ab3ba1
aland1013 <adam.land@gmail.com>
2020-05-13 01:04:08 -0400

    made line on chart change color when dark mode is toggled for stretch

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import moment from "moment";
+import React from 'react';
+import moment from 'moment';
 import {
   LineChart,
   Line,
@@ -7,7 +7,7 @@ import {
   XAxis,
   YAxis,
   Tooltip
-} from "recharts";
+} from 'recharts';
 
 const Chart = ({ sparklineData }) => {
   const formattedData = sparklineData
@@ -15,20 +15,25 @@ const Chart = ({ sparklineData }) => {
       if (idx % 6 === 0) {
         const timeToSubtract = 168 - idx;
         const date = moment()
-          .subtract(timeToSubtract, "hours")
-          .format("ddd h:mma");
+          .subtract(timeToSubtract, 'hours')
+          .format('ddd h:mma');
         return { value: price, date };
       } else if (idx === sparklineData.length - 1) {
-        const date = moment().format("ddd h:mma");
+        const date = moment().format('ddd h:mma');
         return { value: price, date };
       }
       return null;
     })
-    .filter(data => data);
+    .filter((data) => data);
 
   return (
     <LineChart width={1100} height={300} data={formattedData}>
-      <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      <Line
+        className="chart-line"
+        type="monotone"
+        dataKey="value"
+        stroke="#8884d8"
+      />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <XAxis dataKey="date" interval={3} />
       <YAxis />
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -9,11 +9,14 @@ const useDarkMode = () => {
 
   useEffect(() => {
     const body = document.querySelector('body');
+    const lines = document.querySelectorAll('.chart-line path');
 
     if (isDarkModeEnabled) {
       body.classList.add('dark-mode');
+      lines.forEach((line) => line.setAttribute('stroke', 'red'));
     } else {
       body.classList.remove('dark-mode');
+      lines.forEach((line) => line.setAttribute('stroke', '#8884d8'));
     }
   }, [isDarkModeEnabled]);
 
